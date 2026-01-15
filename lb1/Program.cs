4843e9b4adbff0df169b48e52799920e96a9cf89
ads58 <ads58@W10792>
2026-01-15 17:57:44 +0700

    feature(Program): добавлен метод из пункта 5

--- lb1/Program.cs
+++ lb1/Program.cs
@@ -71,6 +71,12 @@ namespace lb1
             PrintPerson(personFromConsole);
             PrintList(list1, "Список 1 после добавления");
             WaitForKey();
+
+            // Создание случайного человека
+            Person randomPerson = GetRandomPerson(); // Вызываем метод из Program
+            Console.WriteLine("\nСоздан случайный человек:");
+            PrintPerson(randomPerson);
+            WaitForKey();
         }
 
         /// <summary>
@@ -126,6 +132,43 @@ namespace lb1
             Console.Write("Пол (0 = мужской, 1 = женский): ");
             int genderInput = int.Parse(Console.ReadLine()!);
             Gender gender = (Gender)genderInput;
+
+            return new Person(name, surname, age, gender);
+        }
+
+        /// <summary>
+        /// Создаёт и возвращает новый экземпляр класса Person с случайными данными.
+        /// </summary>
+        /// <returns>Новый объект Person.</returns>
+        public static Person GetRandomPerson()
+        {
+            Random random = new Random();
+
+            string[] maleNames = { "Александр", "Дмитрий", "Иван", "Сергей", 
+                                   "Андрей", "Максим", "Егор", "Артём" };
+            string[] femaleNames = { "Ангелина", "Елена", "Мария", "Татьяна", 
+                                     "Наталья", "Ольга", "Дарья", "Полина" };
+
+            string[] surnamesMale = { "Склярук", "Смирнов", "Кузнецов", 
+                                      "Попов", "Волков", "Соколов", 
+                                      "Лебедев", "Морозов" };
+            string[] surnamesFemale = { "Савосто", "Смирнова", "Кузнецова", 
+                                        "Попова", "Волкова", "Соколова", 
+                                        "Лебедева", "Морозова" };
+
+            var gender = random.Next(2) == 0 
+                ? Gender.Male
+                : Gender.Female;
+
+            int age = random.Next(0, 124);
+
+            string name = gender == Gender.Male
+                ? maleNames[random.Next(maleNames.Length)]
+                : femaleNames[random.Next(femaleNames.Length)];
+
+            string surname = gender == Gender.Male
+                ? surnamesMale[random.Next(surnamesMale.Length)]
+                : surnamesFemale[random.Next(surnamesFemale.Length)];
 
             return new Person(name, surname, age, gender);
         }
